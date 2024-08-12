af312da8beee69a26b488977410360449a5ebc8e
Luanny Martins <108303052+Luaanny@users.noreply.github.com>
2024-08-12 01:45:17 +0000

    login/componentes

--- app.py
+++ app.py
@@ -20,16 +20,24 @@ def dicas():
 def login():
     return render_template('login.html')
 
-@app.route('/autenticar', methods=['POST'])
-def autenticar():
+@app.route('/Pagina_inicial', methods=['POST'])
+def Pagina_inicial():
     usuario = request.form['usuario']
     senha = request.form['senha']
     if usuario == 'admin' and senha == 'senha123':
-        return 'Bem Vindo'
+        return render_template ('index.html')
     else: 
         flash('Dados incorretos. Login ou senha inválidos', 'danger')
         flash('tente novamente', 'warning')
         return redirect(url_for('login'))
 
+@app.route('/componentes')
+def componentes():
 
-    
+    adms = [{'nome': 'Ingrid Leticia','imagem': 'ingrid.jpg'},
+              {'nome': 'Jonas Nogueira','imagem': 'jonas.jpg'},
+              {'nome': 'Luanny Martins','imagem': 'lua.jpg'},
+              {'nome': 'Ryan Kauê','imagem': 'ryan.jpg'},
+            ]
+    return render_template('componentes.html', adms=adms)
+   
