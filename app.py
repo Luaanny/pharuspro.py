988f2f4283efd72938b4454ab6347dea9206199e
davi <davibezerra123457@gmail.com>
2024-11-05 19:07:47 -0300

    fix: changes the flash messages to make them prettier and more responsible

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lualinde'
 
@@ -9,8 +8,6 @@ app.config['SECRET_KEY'] = 'lualinde'
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 @app.route('/dicas')
 def dicas():
@@ -21,8 +18,9 @@ def dicas():
         "Aproveite a luz natural.",
         "Mantenha portas e janelas fechadas quando o ar-condicionado estiver ligado.",
     ]
-    
+
     return render_template('dicas.html', tips=tips)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,12 +30,13 @@ def login():
 
         if email == "usuario@example.com" and senha == "senha123":
             session['logged_in'] = True
-            flash('Login realizado com sucesso!', 'sucess')
+            flash('Login realizado com sucesso!', 'success')
             return redirect(url_for('index'))
-        
-        flash('Nome de úsuario ou senha incorretos', 'danger')
-            
+
+        flash('Nome de usuário ou senha incorretos', 'error')
+
     return render_template('login.html')
+
 
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
@@ -46,41 +45,48 @@ def cadastrar():
         email = request.form['email']
         senha = request.form['senha']
 
-        flash('Cadastro realizado com sucesso!', 'sucess')
+        flash('Cadastro realizado com sucesso!', 'success')
         return redirect(url_for('login'))
     return render_template('cadastro.html')
- 
+
 
 @app.route('/componentes')
 def componentes():
-
-    adms = [{'nome': 'Ingrid Leticia','imagem': 'ingrid.jpg'},
-              {'nome': 'Jonas Nogueira','imagem': 'jonas.jpg'},
-              {'nome': 'Luanny Martins','imagem': 'lua.jpg'},
-              {'nome': 'Ryan Kauê','imagem': 'ryan.jpg'},
-            ]
+    adms = [
+        {'nome': 'Ingrid Leticia', 'imagem': 'ingrid.jpg'},
+        {'nome': 'Jonas Nogueira', 'imagem': 'jonas.jpg'},
+        {'nome': 'Luanny Martins', 'imagem': 'lua.jpg'},
+        {'nome': 'Ryan Kauê', 'imagem': 'ryan.jpg'},
+    ]
     return render_template('componentes.html', adms=adms)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 @app.route('/metas')
 def metas():
-    return render_template('metas.html') 
+    return render_template('metas.html')
+
 
 @app.route('/ideal_de_consumo')
 def ideal_de_consumo():
     return render_template('ideal_de_consumo.html')
 
+
 @app.route('/logout')
 def logout():
     session.clear()
-    flash('voce foi deslogado', 'sucess')
+    flash('Você foi deslogado', 'success')
     return redirect(url_for('login'))
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
