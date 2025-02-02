87ec6bb7688e0a891d11af4db75b3749db63f310
Luaanny <silva.luanny@escolar.ifrn.edu.br>
2025-02-02 17:49:38 -0300

    simulador ficando pronto

--- app/views/post_routes.py
+++ app/views/post_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, redirect, url_for, request
 from app.models import User
 
 post_bp = Blueprint('post', __name__)
@@ -7,9 +7,43 @@ post_bp = Blueprint('post', __name__)
 def index():
     return render_template("pages/dashboard.html", include_sidebar = True, include_header = True)
 
-@post_bp.route('/metas')
+@post_bp.route('/metas', methods=['GET', 'POST'])
 def metas():
     return render_template('pages/metas.html', include_sidebar=True, include_header=True)
+
+@post_bp.route('/simulador', methods=['POST', 'GET'])
+def simulador():
+    consumo_mensal = None
+
+    if request.method == 'POST':
+        try:
+            potency = float(request.form['potency'])
+            time_interval = float(request.form['time_interval'])
+            tariff = float(request.form['tariff'])
+
+            consumo_mensal = ((potency * time_interval * 30) / 1000) * tariff
+
+            novo_consumo = Consumo(
+                date=datetime.now(),
+                potency=potency,
+                time_interval=time_interval,
+                tariff=tariff,
+                user=current_user.id,
+                consumo_mensal=consumo_mensal,
+            )
+
+            db.session.add(novo_consumo)
+            db.session.commit()
+
+            print(f"Consumo mensal calculado e armazenado na sessão: {consumo_mensal}")
+
+            return redirect(url_for('post.metas'))
+
+        except ValueError:
+            return render_template('pages/simulador.html',
+                                   message="Por favor, insira valores válidos para todos os campos.")
+
+    return render_template('pages/simulador.html')
 
 @post_bp.route('/ideal_de_consumo')
 def ideal_de_consumo():
