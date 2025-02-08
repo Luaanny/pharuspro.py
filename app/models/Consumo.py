90f662170d344aeecb7ff28ae65fc11eeee0274d
davi <davibezerra123457@gmail.com>
2025-02-08 20:48:25 -0300

    adiciona coluna aparelho na tabela consumo

--- app/models/Consumo.py
+++ app/models/Consumo.py
@@ -5,8 +5,8 @@ class Consumo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # Define valor padrão
     potency = db.Column(db.Float, nullable=False)
+    aparelho = db.Column(db.String(80), nullable=False)
     time_interval = db.Column(db.Float, nullable=False)
-    tariff = db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', name='fk_consumo_user',ondelete="CASCADE"), nullable=False)
     consumo_mensal = db.Column(db.Float, nullable=False)
 
