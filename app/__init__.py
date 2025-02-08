ac7a723c4eab90c415c6a3814392b9a426e89b69
davi <davibezerra123457@gmail.com>
2025-02-08 20:48:06 -0300

    importa blueprints de user e consumo no app

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.extensions.database import db
 from app.extensions.migrate import migrate
 from app.extensions.flask_login import lm
 
+
 def create_app(config_class="app.config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
@@ -11,10 +12,15 @@ def create_app(config_class="app.config.DevelopmentConfig"):
     migrate.init_app(app, db)
     lm.init_app(app)
 
+    from app.Controllers.consumo_controller import consumo_bp
+    from app.Controllers.user_controller import user_bp
     from app.views.auth_routes import auth_bp
     from app.views.post_routes import post_bp
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(post_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(consumo_bp)
 
     from app.error_handlers import register_error_handlers
     register_error_handlers(app)
