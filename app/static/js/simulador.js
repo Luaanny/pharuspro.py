84d789fca33b2fbded7f1a4e681618eb77a387e2
Luaanny <silva.luanny@escolar.ifrn.edu.br>
2025-02-23 15:44:38 -0300

    conserta a listagem de aparelhos

--- app/static/js/simulador.js
+++ app/static/js/simulador.js
@@ -1,8 +1,18 @@
 const devicesToggle = document.querySelector('#devicesToggle')
 const devicesList = document.querySelector("#devicesList")
+const cardListCLose = document.querySelector('#cardListClose')
 
 function showDevices () {
-    devicesList.style.transform = 'translate(0, -50%)'
+    devicesList.classList.add('enter')
+    devicesList.classList.remove('exit')
+    devicesToggle.classList.add('d-none')
+}
+
+function hideDevices () {
+    devicesList.classList.add('exit')
+    devicesList.classList.remove('enter')
+    devicesToggle.classList.remove('d-none')
 }
 
 devicesToggle.addEventListener('mouseover', showDevices)
+cardListCLose.addEventListener('click', hideDevices)
